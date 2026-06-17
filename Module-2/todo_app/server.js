c93f80ed351871124afa61440e56506c062dd884
sheikh-riyadh <sheikhriyad350883@gmail.com>
2026-06-17 10:01:02 +0600

    learning pure nodejs

--- Module-2/todo_app/server.js
+++ Module-2/todo_app/server.js
@@ -1,8 +1,12 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const { urlToHttpOptions } = require("url");
 
 const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "./db/todos.json");
+   const url = new URL(req.url, `http://${req.headers.host}`)
+
   if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, {
       "content-type": "aplication/json",
@@ -17,7 +21,6 @@ const server = http.createServer((req, res) => {
       "content-type": "aplication/json",
     });
 
-    const filePath = path.join(__dirname, "./db/todos.json");
     const data = fs.readFileSync(filePath, { encoding: "utf-8" });
 
     res.end(data);
@@ -30,16 +33,30 @@ const server = http.createServer((req, res) => {
 
     req.on("end", () => {
       const todos = JSON.parse(data);
-      console.log({...todos, createtAt})
+      const allTodos = JSON.parse(
+        fs.readFileSync(filePath, { encoding: "utf-8" }),
+      );
+      allTodos.push({ ...todos, createtAt });
+      fs.writeFileSync(filePath, JSON.stringify(allTodos, null, 2), {
+        encoding: "utf-8",
+      });
+      res.end(JSON.stringify({ ...todos, createtAt }, null, 2));
     });
-    res.end("Created todos sucessfully");
+  } else if (req.method === "GET" && req.url.startsWith("/todo")) {
+   
+    console.log(url)
+    const title = req.url.slice(12);
+    const allTodos = JSON.parse(
+      fs.readFileSync(filePath, { encoding: "utf-8" }),
+    );
+    const targetedTodos = allTodos.find((todo) => todo.title === title);
+    res.end(JSON.stringify(targetedTodos, null, 2));
   } else if (req.method === "PATCH" && req.url === "/update-todos") {
     res.end("Update todos successfully");
   } else if (req.method === "DELETE" && req.url === "/delete-todos") {
     res.end("delete todos successfully");
   } else {
     res.end("No route found");
-    console.log(req.url);
   }
 });
 
